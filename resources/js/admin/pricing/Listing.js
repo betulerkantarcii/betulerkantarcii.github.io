34ba1a0107ea44602b0f03b3c34d73467f9940d9
Betül Erkantarcı <betule_98@hotmail.com>
2022-09-02 17:11:21 +0300

    yenilendi

--- resources/js/admin/pricing/Listing.js
+++
@@ -1,5 +0,0 @@
-import AppListing from '../app-components/Listing/AppListing';
-
-Vue.component('pricing-listing', {
-    mixins: [AppListing]
-});
--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.app')
-
-@section('content')
-
-    @include('partials.platform')
-    @include('partials.design')
-    @include('partials.pricing')
-
-@endsection
